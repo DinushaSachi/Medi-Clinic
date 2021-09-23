452589056b25c301cc8ffda06531626d04260b3b
Sachin209 <It20086220@my.sliit.lk>
2021-09-23 15:43:49 +0530

    create doctor profile

---
+++ app/src/main/java/com/example/mediclinic/CreateDoctorProfileActivity.java
@@ -0,0 +1,14 @@
+package com.example.mediclinic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateDoctorProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_doctor_profile);
+    }
+}
