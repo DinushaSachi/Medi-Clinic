ab1a400764f4f4681c9b8c2f738d32aa982996f5
Sachin209 <It20086220@my.sliit.lk>
2021-09-25 22:48:40 +0530

    patient and doctor view profiles

---
+++ app/src/main/java/com/example/mediclinic/DoctorViewProfile.java
@@ -0,0 +1,14 @@
+package com.example.mediclinic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DoctorViewProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor_view_profile);
+    }
+}
