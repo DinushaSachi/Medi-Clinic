7eb51f62b12fcaf6c9d21cc0806ae1b3b2922494
Dinusha Elamaldeniya <it20088828@my.sliit.lk>
2021-09-26 09:24:10 +0530

    create reminder patient

---
+++ app/src/main/java/com/example/mediclinic/reminderPatient.java
@@ -0,0 +1,14 @@
+package com.example.mediclinic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class reminderPatient extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reminder_patient);
+    }
+}
